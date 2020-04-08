fa46c4ee1a18ac77a9af4e004af2c763131a0116
韩帆 <877780530@qq.com>
2020-04-08 21:01:29 +0800

    创建第二个gitTest

---
+++ git_test/src/com/itheima/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.itheima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        int i =1 ;
+        System.out.println(i);
+        System.out.println(i+1);
+    }
+}
